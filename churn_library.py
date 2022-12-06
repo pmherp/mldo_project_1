04e5acda92a425c9a2919a9f2b85f48247fd1c0e
Philip Herp <philipherp@MacBook-Pro-von-Philip.fritz.box>
2022-12-06 17:20:43 +0100

    feat: started populating churn_library.py

--- churn_library.py
+++ churn_library.py
@@ -5,18 +5,60 @@
 import os
 os.environ['QT_QPA_PLATFORM']='offscreen'
 
+import shap
+import joblib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
 
-def import_data(pth):
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import plot_roc_curve, classification_report
+
+import logging
+
+logging.basicConfig(
+        filename='./logs/test_results.log',
+        level=logging.INFO,
+        filemode='w',
+        format='%(name)s - %(levelname)s - %(message)s'
+)
+
+def import_data(pth='./data/bank_data.csv'):
     '''
     returns dataframe for the csv found at pth
 
     input:
-            pth: a path to the csv
+            pth: (str) a path to the csv
     output:
             df: pandas dataframe
-    '''	
-	pass
+    '''
+    try:
+        #test for filepath dtype string
+        assert isinstance(pth, str)
+
+        #test if file exists in path
+        assert os.path.exists(pth)
+
+        #log success messages
+        logging.info('SUCCESS: fileinput is of type string and file exists at given path')
+
+        #read in csv file from given path and store in pandas dataframe
+        df = pd.read_csv(pth)
+
+        return df
+    except AssertionError:
+        logging.error('ERROR: input is not of dtype string or file does not exist')
+
+        return None
+
+    
 
 
 def perform_eda(df):
@@ -28,7 +70,27 @@ def perform_eda(df):
     output:
             None
     '''
-	pass
+    try:
+        #test for df dtype DataFrame
+        assert isinstance(df, pd.DataFrame)
+
+        #logging success message
+        logging.info('SUCCESS: df is of type pd.DataFrame')
+
+        #get shape of df
+        print(f'The dataframe has {df.shape[0]} rows and {df.shape[1]} columns')
+
+        #get sum of empty rows for each column
+        print(f'Sum of empty rows for each column in df:\n{df.isnull().sum()}')
+
+        #get description for df
+        print(f'Basic description for each column in df:\n{df.describe()}')
+    except AssertionError:
+        logging.error('ERROR: getting shape of dataframe failed. Wrong dtype.')
+
+    try:
+        df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    except AssertionError:
 
 
 def encoder_helper(df, category_lst, response):
